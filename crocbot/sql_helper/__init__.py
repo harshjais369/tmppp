73bcc7705d8601c20093b37549228dfc9738da36
Harsh Jaiswal <harshjais369@gmail.com>
2023-03-29 05:11:38 +0530

    Update __init__.py

--- crocbot/sql_helper/__init__.py
+++ crocbot/sql_helper/__init__.py
@@ -1,9 +1,7 @@
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-load_dotenv(verbose=True)
 
 DB_URI = os.environ.get("DATABASE_URL", None)
 
